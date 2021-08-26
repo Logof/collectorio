dcfbf422b2f273941910aa294866683cd7f66e33
Semen Skibin <skibin991@gmail.com>
2021-08-26 15:43:40 +0300

    First commit

---
+++ src/main/java/com/itransition/courses/collectorio/CollectorioApplication.java
@@ -0,0 +1,13 @@
+package com.itransition.courses.collectorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectorioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectorioApplication.class, args);
+    }
+
+}
